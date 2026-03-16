b1f92199f7a34daea0cfff18622eae2d4800eed2
afonso <afonsoaguilar2006@gmail.com>
2026-03-16 21:43:37 +0000

    cors installation

--- src/server.js
+++ src/server.js
@@ -2,6 +2,7 @@ require('dotenv').config()
 const express = require("express");
 const app = express();
 const db = require("../src/config/db")
+const cors = require("cors");
 
 
 
@@ -10,9 +11,13 @@ const {errorHandler} = require("./middlewares/middleWares")
 
 app.use(express.json());
 
+app.use(cors());
+
 app.use("/expenses", expensesRoutes);
 
 app.use(errorHandler);
+
+
 
 db.connectDB();
 
