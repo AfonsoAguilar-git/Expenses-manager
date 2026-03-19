e4401064bf7d7cfdd95dab642c3a4b3362f594eb
afonso <afonsoaguilar2006@gmail.com>
2026-03-19 21:52:59 +0000

    add expense form with post

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,15 +1,55 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
+import Form from "./components/ExpenseForm"
+
 
 function App() {
   const [expenses, setExpenses] = useState([])
   const [loading, setLoading] = useState(true)
   const [error, setError] = useState(null)
+
+  useEffect(()=>{
+    async function fetchExpense(){
+      try{
+        let response = await fetch("http://localhost:3000/expenses")
+        const data = await response.json();
+        setExpenses(data);
+        setLoading(false);
+      }catch(err){
+        setError(err);
+        setLoading(false);
+      }
+    }
+    fetchExpense();
+  },[])
   
-  return (
+  if (loading) return (
     <div>
-      <h1>Expense Tracker</h1>
+      <p>loading ....</p>
     </div>
   )
+  if (error) return (
+    <div>
+      <h1>Error: {error} </h1>
+    </div>
+  )
+  if (expenses) return (
+    <div>
+      <h1>Expense Tracker</h1>
+      {expenses.map(expense => (
+        <div key={expense._id}>
+        <p>{expense.description}</p>
+        <p>{expense.amount}</p>
+        <p>{expense.category}</p>
+        </div>
+      ))}
+      <div>
+        <Form setExpenses={setExpenses} />
+      </div>
+    </div>
+  )
+
+
+  
 }
 
 export default App
---
+++ client/src/components/ExpenseForm.jsx
@@ -0,0 +1,53 @@
+import { useState } from "react"
+
+
+
+
+
+function Form({setExpenses}){
+    const [amount ,setAmount] = useState("");
+    const [description ,setDescription] = useState("");
+    const [category ,setCategory] = useState("Food");
+    async function handleSubmit(e){
+        try{
+        e.preventDefault();
+        const response = await fetch("http://localhost:3000/expenses",
+            {
+                headers:{
+                    "Content-Type": "application/json"
+                },
+                method: "POST",
+                body: JSON.stringify({amount: Number(amount),description,category})
+            })
+            const data = await response.json();
+            setExpenses(prev => [...prev,data]);
+        }catch(err){
+            console.log(err);
+            
+        }
+        
+
+    }
+
+    return (
+        <div>
+            <h1>Add Expense</h1>
+            <form onSubmit={handleSubmit}>
+                <label>Amount</label>
+                <input type="number" value={amount}  onChange={(e) => setAmount(e.target.value)} placeholder="insert amount"/>
+                <label>Description</label>
+                <input type="text" value={description} onChange={(e) => setDescription(e.target.value)} placeholder="insert description"/>
+                <label>Category</label>
+                <select value={category} onChange={(e) => setCategory(e.target.value)}>
+                    <option>Food</option>
+                    <option>House</option>
+                    <option>Work</option>
+                </select>
+                <input type="submit"/>
+                
+            </form>
+        </div>
+    )
+}
+
+export default Form
